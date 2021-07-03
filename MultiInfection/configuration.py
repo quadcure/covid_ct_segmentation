58cc49817f450adae229864b02ba7669bd0e797d
Sahil Uppal <sahiluppal2k@gmail.com>
2021-07-03 12:53:15 +0530

    moving to QuadCure project

--- MultiInfection/configuration.py
+++ MultiInfection/configuration.py
@@ -9,19 +9,19 @@ class Config():
         self.num_classes = 3
 
         # Weights
-        self.inf_net_unet = "https://github.com/saahiluppal/Inf-Net/releases/download/v1.0/unet_model_200.pkl"
+        self.inf_net_unet = "https://storage.googleapis.com/quadcure_api_bucket/weights/unet_model_200.pkl"
 
         # Google Service Account API JSON
-        self.service_account_json = "deployment.json"
+        self.service_account_json = "QuadCureBot.json"
 
         # GC Bucket
-        self.bucket_name = "nosensitivebucket"
-        self.original_images_folder = "images/original"
-        self.infection_images_folder = "images/infection"
-        self.ground_glass_images_folder = "images/ground_glass"
-        self.consolidation_images_folder = "images/consolidation"
+        self.bucket_name = "quadcure_api_bucket"
+        self.original_images_folder = "covid_ctscan/original"
+        self.infection_images_folder = "covid_ctscan/infection"
+        self.ground_glass_images_folder = "covid_ctscan/ground_glass"
+        self.consolidation_images_folder = "covid_ctscan/consolidation"
 
         # Big Query
-        self.project_name = "deployment-317507"
-        self.database_name = "covid_ct"
-        self.table = "single_infection"
+        self.project_name = "trusty-vim-316404"
+        self.database_name = "quadcure_api_query"
+        self.table = "covid_ctscan"
